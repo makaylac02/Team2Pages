81ac9cf9f95ce1db4079cd4afd4e1bb18400a07b
makaylac02 <89802396+makaylac02@users.noreply.github.com>
2024-10-08 09:04:44 -0400

    moved around nodemailer code

--- userValidation.js
+++ userValidation.js
@@ -11,6 +11,38 @@ loginForm.addEventListener("submit", (e) => {
       
       alert("Correct, a verification code has been sent to your email.");
       location=("otherpage.html");
+        /*test*/
+   var nodemailer = require('nodemailer');
+
+   var transporter = nodemailer.createTransport({
+   service: 'gmail',
+   auth: {
+     user: "wrightteam2.137@gmail.com",
+     pass: 'Team2!@Wright'
+   }
+ });
+ 
+ var mailOptions = {
+   from: "wrightteam2.137@gmail.com",
+   to: 'makaylacarr74@gmail.com',
+   subject: 'Sending Email using Node.js',
+   text: 'That was easy!'
+ };
+ 
+ transporter.sendMail(mailOptions, function(error, info){
+   if (error) {
+     console.log(error);
+   } else {
+     console.log('Email sent: ' + info.response);
+   }
+ });
+    
+ 
+ 
+ 
+ 
+ 
+     /*test*/
     } else {
       alert("Wrong");
         
@@ -21,37 +53,6 @@ loginForm.addEventListener("submit", (e) => {
       email.value = "";
     }
 
-   /*test*/
-   var nodemailer = require('nodemailer');
-
-  var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: email,
-    pass: 'Team2!@Wright'
-  }
-});
-
-var mailOptions = {
-  from: email,
-  to: 'makaylacarr74@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-   
-
-
-
-
-
-    /*test*/
+ 
 
   });
