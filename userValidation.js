cd851b5b55463cade160a3cd6b5141dec8820cb4
makaylac02 <89802396+makaylac02@users.noreply.github.com>
2024-10-22 18:32:43 -0400

    removed nodemailer code in userValidation

--- userValidation.js
+++ userValidation.js
@@ -11,37 +11,7 @@ loginForm.addEventListener("submit", (e) => {
       
       alert("Correct, a verification code has been sent to your email.");
       location=("otherpage.html");
-        /*test*/
-
-   const transporter = nodemailer.createTransport({
-   service: 'Gmail',
-   host: "smtp.gmail.com",
-   port: 465,
-   secure: true,
-   auth: {
-     /*user: "wrightteam2.137@gmail.com",
-     pass: 'Team2!@Wright'*/
-     user: "makaylacarr74@gmail.com",
-     pass: "tylc sdni utee vcyl",
-   }
- });
- 
- const mailOptions = {
-   from: "makaylacarr74@gmail.com",
-   to: 'makayla_carr9@yahoo.com',
-   subject: 'Sending Email using Node.js',
-   text: 'That was easy!'
- };
- 
- transporter.sendMail(mailOptions, function(error, info){
-   if (error) {
-     console.log(error);
-   } else {
-     console.log('Email sent: ' + info.response);
-   }
- });
-
-     /*test*/
+   
     } else {
       alert("Wrong");
       password.value = "";
